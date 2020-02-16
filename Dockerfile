03c9573e472974e77579174f94d84106f77367a5
raulanatol <raul@natol.es>
2020-02-16 16:21:54 +0000

    Fix names

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,6 @@ RUN apk add --no-cache python3 && \
     if [ ! -e /usr/bin/python ]; then ln -sf python3 /usr/bin/python ; fi && \
     pip3 install --no-cache --upgrade pip setuptools wheel awscli==${AWSCLI_VERSION}
 
-COPY entrypoint.sh /entrypoint.sh
+ADD entrypoint.sh /entrypoint.sh
 
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,23 @@
-#!/bin/sh -l
+#!/bin/sh
 
-set -eu
+set -e
+
+if [ -z "$AWS_ACCESS_KEY_ID" ]; then
+  echo "AWS_ACCESS_KEY_ID is not set"
+  exit 1
+fi
+
+if [ -z "$AWS_SECRET_ACCESS_KEY" ]; then
+  echo "AWS_SECRET_ACCESS_KEY is not set"
+  exit 1
+fi
+
+if [ -z "$AWS_BUCKET_NAME" ]; then
+  echo "AWS_BUCKET_NAME is not set"
+  exit 1
+fi
+
+echo "Starting..."
 
 mkdir -p ~/.aws
 echo -e "[default]\naws_access_key_id=${AWS_ACCESS_KEY_ID}\naws_secret_access_key=${AWS_SECRET_ACCESS_KEY}" >~/.aws/credentials
@@ -16,7 +33,7 @@ aws s3 sync ${SOURCE} s3://${AWS_BUCKET_NAME}/${TARGET} ${FLAGS}
 if [ -z "${WITH_CLOUD_FRONT_INVALIDATION}" ]; then
   if [ -z "${AWS_CLOUDFRONT_DISTRIBUTION_ID}" ]; then
     echo "Impossible to request an invalidation we need the AWS_CLOUDFRONT_DISTRIBUTION_ID value"
-    exit 3
+    exit 1
   else
     aws cloudfront create-invalidation --distribution-id ${AWS_CLOUDFRONT_DISTRIBUTION_ID} --paths "${TARGET}"
   fi
